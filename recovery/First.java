f920c43bfa819fd7878d6112a17d45113c12511f
Jan Pierry <janpierrycoelho@gmail.com>
2017-10-20 00:25:20 -0200

    falta metodo classbody

--- recovery/First.java
+++ recovery/First.java
@@ -13,19 +13,48 @@ public class First { //implementa os conjuntos first p/ alguns n.terminais
     static public final RecoverySet constructdecl = new RecoverySet();
     static public final RecoverySet statlist = new RecoverySet();
     static public final RecoverySet program = classlist;
+    static public final RecoverySet switchcasestat = new RecoverySet();
 
     static {
         methoddecl.add(new Integer(langXConstants.INT));
         methoddecl.add(new Integer(langXConstants.STRING));
         methoddecl.add(new Integer(langXConstants.IDENT));
+        methoddecl.add(new Integer(langXConstants.BOOLEAN));
+        methoddecl.add(new Integer(langXConstants.CHAR));
+        methoddecl.add(new Integer(langXConstants.DOUBLE));
+        methoddecl.add(new Integer(langXConstants.BYTE));
+        methoddecl.add(new Integer(langXConstants.SHORT));
+        methoddecl.add(new Integer(langXConstants.LONG));
+        methoddecl.add(new Integer(langXConstants.FLOAT));
+        methoddecl.add(new Integer(langXConstants.PUBLIC));
+        methoddecl.add(new Integer(langXConstants.PRIVATE));
+        methoddecl.add(new Integer(langXConstants.PROTECTED));
+        methoddecl.add(new Integer(langXConstants.FINAL));
 
         vardecl.add(new Integer(langXConstants.INT));
         vardecl.add(new Integer(langXConstants.STRING));
         vardecl.add(new Integer(langXConstants.IDENT));
+        vardecl.add(new Integer(langXConstants.BOOLEAN));
+        vardecl.add(new Integer(langXConstants.CHAR));
+        vardecl.add(new Integer(langXConstants.DOUBLE));
+        vardecl.add(new Integer(langXConstants.BYTE));
+        vardecl.add(new Integer(langXConstants.SHORT));
+        vardecl.add(new Integer(langXConstants.LONG));
+        vardecl.add(new Integer(langXConstants.FLOAT));
+        vardecl.add(new Integer(langXConstants.PRIVATE));
+        vardecl.add(new Integer(langXConstants.PUBLIC));
+        vardecl.add(new Integer(langXConstants.PROTECTED));
+        vardecl.add(new Integer(langXConstants.FINAL));
 
         classlist.add(new Integer(langXConstants.CLASS));
+        classlist.add(new Integer(langXConstants.PUBLIC));
+        classlist.add(new Integer(langXConstants.PRIVATE));
+        classlist.add(new Integer(langXConstants.PROTECTED));
 
         constructdecl.add(new Integer(langXConstants.CONSTRUCTOR));
+        constructdecl.add(new Integer(langXConstants.PUBLIC));
+        constructdecl.add(new Integer(langXConstants.PRIVATE));
+        constructdecl.add(new Integer(langXConstants.PROTECTED));
 
         statlist.addAll(vardecl);
         statlist.add(new Integer(langXConstants.IDENT)); // first do atribstat
@@ -38,5 +67,8 @@ public class First { //implementa os conjuntos first p/ alguns n.terminais
         statlist.add(new Integer(langXConstants.LBRACE));
         statlist.add(new Integer(langXConstants.BREAK));
         statlist.add(new Integer(langXConstants.SEMICOLON));
+        statlist.add(new Integer(langXConstants.DO));
+        statlist.add(new Integer(langXConstants.WHILE));
+        statlist.add(new Integer(langXConstants.SWITCH));
     }
 }
